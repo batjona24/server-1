2f87afd5601bb67f211fb39af5c417132d00776c
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-12-04 21:22:45 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import Range from './range.js';
+
+const app = express();
+app.use(express.json());
+
+//GET
+app.get('/range', async (req, res) => {
+    const { numbers } = req.query;
+    const numbers_array = numbers.split(',');
+    if(numbers.length == 2) {
+        const first = Number(numbers_array[0]);
+        const last = Number(numbers_array[1]);
+        const range = Range(first, last);
+        res.status(200);
+        res.json(range);
+    }
+    else {
+        res.status(404);
+        res.json("Please put only two numbers!"); 
+    }
+});
+
+// POST
+app.post('/range', async (req, res) => {
+    const { numbers=[] } = req.body;
+    if(numbers.length == 2) {
+        const first = numbers[0];
+        const last = numbers[1];
+        const range = Range(first, last);
+        res.status(200);
+        res.json(range);
+    }
+    else {
+        res.status(404);
+        res.json("Please put only two numbers!"); 
+    }
+});
+
+app.all('/*', async (req, res) => {
+    res.status(404);
+    res.json({ error: 'This route does not exist' });
+});
+
+const hostname = 'localhost';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+    console.log(`Server listening on http://${hostname}:${port}`)
+});
